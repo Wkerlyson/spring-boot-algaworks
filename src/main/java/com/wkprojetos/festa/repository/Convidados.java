19bc1a0eb239a35baed4f74038c5dcaf3b3d4b0f
Fulano da Silva <wkerlyson.batista@gmail.com>
2017-08-27 16:52:16 -0300

    Utilização do Spring Data JPA e listagem de Objetos com Thymeleaf

---
+++ src/main/java/com/wkprojetos/festa/repository/Convidados.java
@@ -0,0 +1,9 @@
+package com.wkprojetos.festa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wkprojetos.festa.model.Convidado;
+
+public interface Convidados extends JpaRepository<Convidado, Long>{
+
+}
